1fb563da7e9a8b330bd604eba07071fa2e0a4bbd
Nick Cronin <nick@cronin.id.au>
2015-08-09 11:09:19 +1000

    Support posting and updating comments.

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -29,11 +29,25 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var author = React.findDOMNode(this.refs.author).value.trim();
+        var text = React.findDOMNode(this.refs.text).value.trim();
+        if (!text || !author) {
+            return;
+        }
+        this.props.onCommentSubmit({author: author, comment: text});
+        React.findDOMNode(this.refs.author).value = '';
+        React.findDOMNode(this.refs.text).value = '';
+        return;
+    },
     render: function() {
         return (
-            <div className="commentForm">
-                Hello, world! I am a CommentForm.
-            </div>
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Your name" ref="author"/>
+                <input type="text" placeholder="Say something..." ref="text" />
+                <input type="submit" value="Post" />
+            </form>
         );
     }
 });
@@ -42,7 +56,23 @@ var CommentBox = React.createClass({
     getInitialState: function() {
         return {data:[]};
     },
-    componentDidMount: function() {
+    handleCommentSubmit: function(comment) {
+        var config = {
+            url: this.props.url,
+            data: JSON.stringify(comment),
+            type: 'POST',
+            contentType: 'application/json',
+            success: function(data) {
+                debugger;
+            }.bind(this),
+            failure: function(xhr, status, err) {
+                debugger;
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        };
+        $.ajax(config);
+    },
+    loadCommentsFromServer: function() {
         $.ajax({
             url: this.props.url,
             dataType: 'json',
@@ -58,17 +88,21 @@ var CommentBox = React.createClass({
             }.bind(this)
         });
     },
+    componentDidMount: function() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
     render: function() {
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
                 <CommentList data={this.state.data}/>
-                <CommentForm />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
             </div>
         );
     }
 });
 React.render(
-<CommentBox url="/comments"/>,
+<CommentBox url="/comments" pollInterval={2000}/>,
     document.getElementById('content')
 );
